ef053b24ecb3149183943dcd392d3b71f81ce41b
Tom <thenimblade@gmail.com>
2024-03-26 16:31:51 +0800

    Move legacy site to assets and link in new homepage

--- toms-portfolio/src/app/app.module.ts
+++ toms-portfolio/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { HomepageComponent } from './homepage/homepage.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
