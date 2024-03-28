03a2c90f1e29dd408906cb566734eeeb9dd06273
Tom <thenimblade@gmail.com>
2024-03-28 16:49:12 +0800

    Bootstrap socials tooltips

--- toms-portfolio/src/app/homepage/homepage.component.ts
+++ toms-portfolio/src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -7,6 +7,7 @@ import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./homepage.component.scss'],
   standalone: true,
 	imports: [NgbTooltipModule],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomepageComponent implements OnInit {
   ngOnInit(): void {}
