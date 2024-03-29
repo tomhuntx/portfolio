457a591c28c594c11357a8a343008d26a6187b3b
Tom <thenimblade@gmail.com>
2024-03-29 13:49:24 +0800

    Move nav buttons to component

--- toms-portfolio/src/app/homepage/homepage.component.ts
+++ toms-portfolio/src/app/homepage/homepage.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavButtonComponent } from "../nav-button/nav-button.component";
 
 @Component({
-  selector: 'app-homepage',
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss'],
-  standalone: true,
-	imports: [NgbTooltipModule],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-homepage',
+    templateUrl: './homepage.component.html',
+    styleUrls: ['./homepage.component.scss'],
+    standalone: true,
+    encapsulation: ViewEncapsulation.None,
+    imports: [NgbTooltipModule, NavButtonComponent]
 })
 export class HomepageComponent implements OnInit {
   ngOnInit(): void {}
